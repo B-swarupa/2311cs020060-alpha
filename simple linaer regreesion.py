1abac7d989616e62094e1e18afac2e4eb224e8bc
B-swarupa <2311cs020060@mallareddyuniversity.ac.in>
2025-01-31 11:39:06 +0530

    Add files via upload

---
+++ simple linaer regreesion.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
+data1=pd.read_csv("NewspaperData.csv")
+data1
+
+
+# In[3]:
+
+
+data1.info()
+
+
+# In[4]:
+
+
+data1.describe()
+
+
+# In[11]:
+
+
+plt.boxplot(data1["daily"])
+
+
+# In[10]:
+
+
+plt.scatter(data1["daily"],data1["sunday"])
+
+
+# In[15]:
+
+
+data1['daily'].corr(data1['sunday'])
+
+
+# In[16]:
+
+
+model=smf.ols("sunday~daily",data=data1).fit()
+model.summary()
+
+
+# In[18]:
+
+
+x=data1["daily"].values
+y=data1["sunday"].values
+plt.scatter(x,y,color="m",marker="o",s=30)
+b0=13.84
+b1=1.33
+y_hat=b0+b1*x
+plt.plot(x,y_hat,color="g")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
